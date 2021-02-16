d852a4a775cbd3e1d881ebe3298a407ecd670b2b
Ksenia Shabanskaya <shabanskaia.ka@phystech.edu>
2021-02-16 21:28:01 +0300

    added class card

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,90 @@
+class Card {
+	constructor(data, templateSelector) {
+		this._link = data.link;
+		this._name = data.name;
+		this._templateSelector = templateSelector;
+	}
+
+	// метод для получения элемента шаблона
+	_getTemplate() {
+		const templateCard = document.querySelector(this._templateSelector).content.querySelector('.card');
+		const card = templateCard.cloneNode(true);
+		return card
+	}
+
+	// метод возвращающий заполненный элемент карточки со всеми слушателями
+	createCard() {
+		this._element = this._getTemplate()
+		this._cardPicture = this._element.querySelector('.card__pic');
+		this._cardTitle = this._element.querySelector('.card__title');
+
+		this._cardPicture.src = this._link;
+		this._cardPicture.alt = this._name;
+		this._cardTitle.textContent = this._name;
+		this._setEventListeners()
+
+		return this._element;
+	}
+
+	_setEventListeners() {
+		this._likeButton = this._element.querySelector('.card__like');
+		this._trashButton = this._element.querySelector('.card__delete-button');
+
+		this._likeButton.addEventListener('click', () => {
+			this._handleLikeButton();
+		});
+		this._trashButton.addEventListener('click', () => {
+			this._handleDeleteCard();
+		});
+		this._cardPicture.addEventListener('click', () => {
+			this._handlePopupImagePreview();
+		});
+	}
+	
+	//метод управления кнопкой лайка
+	_handleLikeButton() {
+		this._likeButton.classList.toggle('card__like_active');
+	}
+
+	//метод управления кнопкой корзины
+	_handleDeleteCard(evt) {
+		this._cardPicture.closest('.card').remove();
+	}
+
+	//метод управления кликом по карточке
+	_handlePopupImagePreview() {
+		this._popupImage = document.querySelector('.popup_feature_fullview'); 
+		this._imageSubtitle = this._popupImage.querySelector('.popup__subtitle');
+		this._previewPic = this._popupImage.querySelector('.popup__pic');
+		this._imageSubtitle.textContent = this._name
+		this._previewPic.src = this._link;
+		this._previewPic.alt = this._name;
+		this._openPopup();
+	}
+
+	//метод закрытия попапа по кнопке эскейп
+	_closePopupOnEsc(evt) {
+		if (evt.key == 'Escape') {
+			this._closePopup();
+		};
+	}
+
+	//метод для закрытия попапа картинки по эскейпу
+	_closePopup() {
+		this._popupImage.classList.remove('popup_opened');
+		document.removeEventListener('keydown', closePopupOnEsc);
+	}
+
+	//метод для открытия попапа
+	_openPopup(popup) {
+		this._popupImage.classList.add('popup_opened');
+		document.addEventListener('keydown', (evt) => {
+			this._closePopupOnEsc(evt);
+		});
+	}
+}
+
+
 //поиск и определние переменных для формы редактирования профиля 
 const editButton = document.querySelector('.profile__edit-button'); 
 const popupEdit = document.querySelector('.popup_feature_edit');
@@ -24,7 +111,7 @@ const imageSubtitle = popupImage.querySelector('.popup__subtitle');
 const previewPic = popupImage.querySelector('.popup__pic');
 
 const placesWrap = document.querySelector('.places__list'); //контейнер с карточками
-const templateCard = document.querySelector('.template-card').content; //шаблон карточки
+const templateCard = document.querySelector('.template-card').content; //шаблон карточки!!!!!!!!!!!!!!!!!!!!!!!
 
 //поиск оверлеев попапов
 const popupsArray = Array.from(document.querySelectorAll('.popup'));
@@ -97,16 +184,18 @@ function handleAddFormSubmit(evt) {
 //добавление начальных карточек
 function addInitial() {
 	initialCards.forEach((item) => {
-		addCard(item, placesWrap);
+		addCard(item, placesWrap);	
 	});
 }
 
 function addCard(item, wrap) {
-	wrap.prepend(createCard(item));
+	const card = new Card(item, '.template-card');
+	//wrap.prepend(createCard(item));
+	wrap.prepend(card.createCard());
 }
 
 //функция создающая разметку карточки по заданным данным
-function createCard(item) {
+/* function createCard(item) {
 	const card = templateCard.cloneNode(true);
 	const cardPicture = card.querySelector('.card__pic');
 	const likeButton = card.querySelector('.card__like');
@@ -119,26 +208,26 @@ function createCard(item) {
 	trashButton.addEventListener('click', handleDeleteCard);
 	cardPicture.addEventListener('click', () => handlePopupImagePreview(item));
 	return card;
-}
+} */
 
-
+/* 
 //функции для обработки событий с попапом просмотра отдельного фото, закрытие выше
 function handlePopupImagePreview(item) {
 	imageSubtitle.textContent = item.name
 	previewPic.src = item.link;
 	previewPic.alt = item.name;
 	openPopup(popupImage);
-}
+} */
 
 
 //функции дляобработки собятий с кномнами на карточках
-function handleDeleteCard(evt) {
+/* function handleDeleteCard(evt) {
 	evt.target.closest('.card').remove();
-}
+} */
 
-function handleLikeButton(evt) {
+/* function handleLikeButton(evt) {
 	evt.target.classList.toggle('card__like_active');
-}
+} */
 
 //добавление начальных значений формы при загрузке страницы
 function addInitialValues() {
@@ -164,3 +253,5 @@ closeButtonAddForm.addEventListener('click', () => closePopup(popupAdd));
 addFormElement.addEventListener('submit', handleAddFormSubmit);
 
 closeButtonImage.addEventListener('click', () => closePopup(popupImage));
+
+
